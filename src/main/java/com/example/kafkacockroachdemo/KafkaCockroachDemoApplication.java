034b83eaa5110217ae9ed0fed771625cc337f9cf
Dheera Alluri <adheera0691@gmail.com>
2021-11-01 20:18:19 -0400

    project setup

---
+++ src/main/java/com/example/kafkacockroachdemo/KafkaCockroachDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkacockroachdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCockroachDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCockroachDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkacockroachdemo/KafkaCockroachDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkacockroachdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCockroachDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
